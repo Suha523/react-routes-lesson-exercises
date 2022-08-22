6594963c8d503232dfae6e646ee2bdaeefcfe6b6
suha <suhashehahde@gmail.com>
2022-08-22 00:49:02 -0700

    add react-route-exercises

--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
-import '../styles/fentity.css'
-
+import React, { Component } from "react";
+import "../styles/fentity.css";
 
 class Fentity extends Component {
-    render() {
-        return (
-            <div id="creature-container">
-                <h1>{fentity.name}</h1>
-                <img src={fentity.imgUrl} alt=""/>
-                <div className="title">Power:</div>
-                <div className="power text"> {fentity.power}</div>
-                <div className="other text">{fentity.other}</div>
-            </div>
-        )
-    }
+  render() {
+    let fentityCategory = this.props.match.params.fentities;
+    let name = this.props.match.params.name;
+    let fentities = this.props.state[fentityCategory];
+    let fentity = fentities.find((f) => f.name === name);
+    console.log(fentity);
+
+    return (
+      <div id="creature-container">
+        <h1>{fentity.name}</h1>
+        <img src={fentity.imgUrl} alt="" />
+        <div className="title">Power:</div>
+        <div className="power text"> {fentity.power}</div>
+        <div className="other text">{fentity.other}</div>
+      </div>
+    );
+  }
 }
 
 export default Fentity;
